29c7a3958a5e42bde4d86f2d266f6f9429120a0f
SoulSecret <116476415+SoulSecret@users.noreply.github.com>
2022-10-25 01:39:33 +0800

    Add files via upload

---
+++ search.js
@@ -0,0 +1,731 @@
+//Song
+const song = [
+				//PH TOP HITS
+				{
+						
+								img: "imagephtophits/snm.png",
+								title: "Sun and Moon",
+								artist: "Anees",
+								url: "Sun&Moon.html"
+				},
+				{
+					
+								img: "imagephtophits/isalang.jpg",
+								title: "Isa lang",
+								artist: "Arthur Nery",
+								url: "isalang.html"
+				},
+				{
+					
+								img: "imagephtophits/diwata.webp",
+								title: "Diwata",
+								artist: "Sam Concepcion",
+								url: "diwata.html"
+				},
+				{
+					
+								img: "imagephtophits/angwakas.png",
+								title: "Ang Wakas",
+								artist: "Arthur Miguel",
+								url: "angwakas.html"
+				},
+				{
+			    
+								img: "imagephtophits/ghost.png",
+								title: "Ghost",
+								artist: "Justin Bieber",
+								url: "ghost.html"
+				},
+				{
+						
+								img: "imagephtophits/line.jpg",
+								title: "Line Without a Hook",
+								artist: "Ricky Montgomery",
+								url: "line-without-a-hook.html"
+				},
+				{
+						
+								img: "imagephtophits/ikawlang.jpg",
+								title: "Ikaw Lang",
+								artist: "Kiyo",
+								url: "ikawlang.html"
+				},
+				{
+					
+								img: "imagephtophits/unangsayaw.jpg",
+								title: "Unang Sayaw",
+								artist: "Nobita",
+								url: "unangsayaw.html"
+				},
+				{
+					
+								img: "imagephtophits/tahanan.jpg",
+								title: "Tahanan",
+								artist: "Adie",
+								url: "tahanan.html"
+				},
+				{
+					
+								img: "imagephtophits/neverenough.jpg",
+								title: "Never Enough",
+								artist: "Loren Allred",
+								url: "NerverEnough.html"
+				},
+				{
+					
+								img: "imagephtophits/uify.jpg",
+								title: "Until I Found You",
+								artist: "Stephen Sanchez",
+								url: "until-i-found-you.html"
+				},
+				{
+				
+								img: "imagephtophits/uify.png",
+								title: "Every Summertime",
+								artist: "Niki",
+								url: "every-summertime.html"
+				},
+				//OPM Music
+
+				{
+						
+								img: "imageopm/lambing.png",
+								title: "Lambing",
+								artist: "Matthaios",
+								url: "lambing.html"
+				},
+				{
+						
+								img: "imageopm/paraluman.jpg",
+								title: "Paralumam",
+								artist: "Adie",
+								url: "paraluman.html"
+				},
+				{
+						
+								img: "imageopm/luha.jpg",
+								title: "Luha",
+								artist: "Aegis",
+								url: "luha.html"
+				},
+				{
+						
+								img: "imageopm/sabihinmona.jpg",
+								title: "Sabihin Mo Na",
+								artist: "Top Suzara",
+								url: "sabihinmona.html"
+				},
+				{
+						
+								img: "imageopm/walangiba.jpg",
+								title: "Walang Iba",
+								artist: "Ezra Band",
+								url: "walangiba.html"
+				},
+				{
+						
+								img: "imageopm/kungwalaka.jpg",
+								title: "Kung Wala Ka",
+								artist: "Hale",
+								url: "kungwalaka.html"
+				},
+				{
+						
+								img: "imageopm/dimoalam.jpg",
+								title: "Di Mo Alam",
+								artist: "December Avenue",
+								url: "dimoalam.html"
+				},
+				{
+						
+								img: "imageopm/buko.jpg",
+								title: "Buko",
+								artist: "Jireh Lim",
+								url: "buko.html"
+				},
+				{
+						
+								img: "imageopm/ligaya.jpg",
+								title: "Ligaya",
+								artist: "Mrld",
+								url: "ligaya.html"
+				},
+				{
+						
+								img: "imageopm/ikawlamang.jpg",
+								title: "Ikaw Lamang",
+								artist: "Silent Sanctuary",
+								url: "ikaw-lang.html"
+				},
+				{
+						
+								img: "imageopm/nobela.png",
+								title: "Nobela",
+								artist: "Join The Club",
+								url: "nobela.html"
+				},
+				{
+						
+								img: "imageopm/demonyo.jpg",
+								title: "Demonyo",
+								artist: "Juan Karlos Labajo",
+								url: "demonyo.html"
+				},
+		//RnB
+				{
+						
+								img: "RnB/angel-baby.jpg",
+								title: "Angel Baby",
+								artist: "Troye Sivan",
+								url: "angel-baby.html"
+				},
+				{
+						
+								img: "RnB/higa.jpg",
+								title: "Higa",
+								artist: "Arthur Nery",
+								url: "higa.html"
+				},
+				{
+						
+								img: "RnB/double-take.jpg",
+								title: "Double Take",
+								artist: "Dhruv",
+								url: "double-take.html"
+				},
+				{
+						
+								img: "RnB/samahanmoako.jpg",
+								title: "Samahan Mo Ako",
+								artist: "Hey its Je",
+								url: "samahanmoako.html"
+				},
+				{
+						
+								img: "RnB/urong-sulong.jpg",
+								title: "Urong Sulong",
+								artist: "Kiyo",
+								url: "urong-sulong.html"
+				},
+				{
+						
+								img: "RnB/tunay.jpg",
+								title: "Tunay",
+								artist: "Lance Santidas",
+								url: "tunay.html"
+				},
+				{
+						
+								img: "RnB/kungpwedelang.jpg",
+								title: "Kung Pwede Lang",
+								artist: "Emman",
+								url: "kungpwedelang.html"
+				},
+				{
+						
+								img: "RnB/pano.jpg",
+								title: "Pano",
+								artist: "Zack Tabudlo",
+								url: "pano.html"
+				},
+				{
+						
+								img: "RnB/lamf.jpg",
+								title: "Like All My Friends",
+								artist: "Francis Karel",
+								url: "like-all-my-friends.html"
+				},
+				{
+						
+								img: "RnB/sasusunodnalang.jpg",
+								title: "Sa Susunod Nalang",
+								artist: "Skusta Clee",
+								url: "sa-susunod-nalang.html"
+				},
+				{
+						
+								img: "RnB/traitor.png",
+								title: "Traitor",
+								artist: "Olivia Rodrigo",
+								url: "traitor.html"
+				},
+				{
+						
+								img: "RnB/isapa.png",
+								title: "Isa Pa",
+								artist: "Because",
+								url: "isapa.html"
+				},
+				//Rock
+				{
+						
+								img: "imagerock/Marty-nyebera.jpg",
+								title: "Martyr Nyebera",
+								artist: "Kamikazee",
+								url: "martyr-nyebera.html"
+				},
+				{
+						
+								img: "imagerock/hallelujah.jpg",
+								title: "Hallelujah",
+								artist: "Bamboo",
+								url: "hallelujah.html"
+				},
+				{
+						
+								img: "imagerock/parasayo.jpg",
+								title: "Para Sayo",
+								artist: "Parokya ni Edgar",
+								url: "para-sayo.html"
+				},
+				{
+						
+								img: "imagerock/baliw.jpg",
+								title: "Baliw",
+								artist: "COLN",
+								url: "baliw.html"
+				},
+				{
+						
+								img: "imagerock/heaven-knows.jpg",
+								title: "Heaven Knows",
+								artist: "Orange & Lemons",
+								url: "heaven-knows.html"
+				},
+				{
+						
+								img: "imagerock/borrwed-time.jpg",
+								title: "Borrowed Time",
+								artist: "Cueshé",
+								url: "borrowed-time.html"
+				},
+				{
+						
+								img: "imagerock/magpakailanman.jpg",
+								title: "Magpakailanman",
+								artist: "Rocksteddy",
+								url: "magpakailamman.html"
+				},
+				{
+						
+								img: "imagerock/kisapmata.jpg",
+								title: "Kisapmata",
+								artist: "Rivermaya",
+								url: "kisapmata.html"
+				},
+				{
+						
+								img: "imagerock/paanosasabihin.jpg",
+								title: "Paano Sasabihin",
+								artist: "Join The Club",
+								url: "paano-sasabihin.html"
+				},
+				{
+						
+								img: "imagerock/beer.png",
+								title: "Beer",
+								artist: "Itchyworms",
+								url: "beer.html"
+				},
+				{
+						
+								img: "imagerock/noypi.jpg",
+								title: "Noypi",
+								artist: "Bamboo",
+								url: "noypi.html"
+				},
+				{
+						
+								img: "imagerock/sundo.png",
+								title: "Sundo",
+								artist: "Imago",
+								url: "sundo.html"
+				},
+				//Reggae
+				{
+						
+								img: "imagereggae/lintik.jpg",
+								title: "Lintik",
+								artist: "Brownman Revival",
+								url: "lintik.html"
+				},
+				{
+						
+								img: "imagereggae/dikdyonaryo.jpg",
+								title: "Diksyonaryo",
+								artist: "The Chongkeys",
+								url: "diksyonaryo.html"
+				},
+				{
+						
+								img: "imagereggae/sabihinmo.jpg",
+								title: "Sabihin Mo",
+								artist: "Hemp Republic",
+								url: "sabihinmo.html"
+				},
+				{
+						
+								img: "imagereggae/ilalim.jpg",
+								title: "Ilalim",
+								artist: "Chocolate Factory",
+								url: "ilalim.html"
+				},
+				{
+						
+								img: "imagereggae/Filipina.jpg",
+								title: "Filipina Island Gyal",
+								artist: "Jeck Pipil & Peacepipe",
+								url: "filipina.html"
+				},
+				{
+						
+								img: "imagereggae/malingakala.jpg",
+								title: "Maling Akala",
+								artist: "Brownman Revival",
+								url: "maling-akala.html"
+				},
+				{
+						
+								img: "imagereggae/modelo.jpg",
+								title: "Modelong Charing",
+								artist: "blakdyak",
+								url: "modelong-charing.html"
+				},
+				{
+						
+								img: "imagereggae/usok.jpg",
+								title: "Usok",
+								artist: "Asin",
+								url: "usok.html"
+				},
+				{
+						
+								img: "imagereggae/mountain.jpg",
+								title: "Baby I Love Your Way",
+								artist: "Big Mountain",
+								url: "baby-i-love-your-way.html"
+				},
+				{
+						
+								img: "imagereggae/bilog.jpg",
+								title: "Bilog Nanaman Ang Buwan",
+								artist: "Tropical Depression",
+								url: "bilog-nanaman-ang-buwan.html"
+				},
+				{
+						
+								img: "imagereggae/tignanmo.jpg",
+								title: "Tignan Mo",
+								artist: "The Chongkeys",
+								url: "tignan-mo.html"
+				},
+				{
+						
+								img: "imagereggae/heartstring.jpg",
+								title: "Heartstring",
+								artist: "kolohe kai",
+								url: "heartstring.html"
+				},
+				//Pop Music
+				{
+						
+								img: "imagepop/easy.png",
+								title: "Easy on Me",
+								artist: "Adele",
+								url: "easy-on-me.html"
+				},
+				{
+						
+								img: "imagepop/pk.png",
+								title: "Paninindigan Kita",
+								artist: "Ben&Ben",
+								url: "paninindigan-kita.html"
+				},
+				{
+						
+								img: "imagepop/crazier.jpg",
+								title: "Crazier",
+								artist: "Taylor Swift",
+								url: "crazier.html"
+				},
+				{
+						
+								img: "imagepop/bone.jpg",
+								title: "To The Bone",
+								artist: "Pamumgkas",
+								url: "to-the-bone.html"
+				},
+				{
+						
+								img: "imagepop/party.jpg",
+								title: "Party in the USA",
+								artist: "Miley Cyrus",
+								url: "party.html"
+				},
+				{
+						
+								img: "imagepop/lapis.jpg",
+								title: "Sana'y Di Nalang",
+								artist: "Bandang Lapis",
+								url: "lapis.html"
+				},
+				{
+						
+								img: "imagepop/hatid.jpg",
+								title: "Hatid",
+								artist: "The Juans",
+								url: "hatid.html"
+				},
+				{
+						
+								img: "imagepop/shivers.png",
+								title: "Shivers",
+								artist: "Ed Sheeran",
+								url: "shivers.html"
+				},
+				{
+						
+								img: "imagepop/butterflies.png",
+								title: "Butterflies",
+								artist: "Abe Parker",
+								url: "butterflies.html"
+				},
+				{
+						
+								img: "imagepop/drinking.png",
+								title: "I Quit Drinking",
+								artist: "Lany & Kelsea Ballerini",
+								url: "i-quit-drinking.html"
+				},
+				{
+						
+								img: "imagepop/letters.jpg",
+								title: "Letters",
+								artist: "Maximillian",
+								url: "letters.html"
+				},
+				{
+						
+								img: "imagepop/igii.jpg",
+								title: "I Guess I'm Inlove",
+								artist: "Clinton Kane",
+								url: "i-guess-im-inlove.html"
+				},
+				//k-pop
+				{
+						
+								img: "k-pop/kill-this-love.jpg",
+								title: "Kill This Love",
+								artist: "BlackPink",
+								url: "kill.html"
+				},
+				{
+						
+								img: "k-pop/ddu.jpg",
+								title: "ddu-du ddu-du",
+								artist: "BlackPink",
+								url: "ddu.html"
+				},
+				{
+						
+								img: "k-pop/dynamite.png",
+								title: "Dynamite",
+								artist: "BTS",
+								url: "dynamite.html"
+				},
+				{
+						
+								img: "k-pop/swipe.png",
+								title: "Swipe",
+								artist: "Itzy",
+								url: "swipe.html"
+				},
+				{
+						
+								img: "k-pop/zoom.png",
+								title: "Zoom",
+								artist: "Jessi",
+								url: "zoom.html"
+				},
+				{
+						
+								img: "k-pop/lalisa.png",
+								title: "Lalisa",
+								artist: "Lisa",
+								url: "lalisa.html"
+				},
+				{
+						
+								img: "k-pop/fancy.png",
+								title: "Fancy",
+								artist: "Twice",
+								url: "fancy.html"
+				},
+				{
+						
+								img: "k-pop/what-is-love.png",
+								title: "What is Love?",
+								artist: "Fancy",
+								url: "what-is-love.html"
+				},
+				{
+						
+								img: "k-pop/fearless.png",
+								title: "Fearless",
+								artist: "Le Sserafim",
+								url: "fearless.html"
+				},
+				{
+						
+								img: "k-pop/but-you.png",
+								title: "But You",
+								artist: "iKon",
+								url: "but-you.html"
+				},
+				{
+						
+								img: "k-pop/lovedive.png",
+								title: "Love Dive",
+								artist: "IVE",
+								url: "love-dive.html"
+				},
+				{
+						
+								img: "k-pop/darari.jpg",
+								title: "Darari",
+								artist: "Treasure",
+								url: "darari.html"
+				},
+			//Worship
+				{
+						
+								img: "imageworship/10k.jpg",
+								title: "10,000 Reasons",
+								artist: "Matt Redman",
+								url: "10,000-reason.html"
+				},
+				{
+						
+								img: "imageworship/alay-ko.jpg",
+								title: "Ang Tanging Alay Ko",
+								artist: "April Boy",
+								url: "ang-tanging-alay-ko.html"
+				},
+				{
+						
+								img: "imageworship/king.jpg",
+								title: "King of Kings",
+								artist: "Hillsong Worship",
+								url: "king-of-kings.html"
+				},
+				{
+						
+								img: "imageworship/leadmelord.jpg",
+								title: "Lead Me Lord",
+								artist: "Aiza Seguerra",
+								url: "lead-me-lord.html"
+				},
+				{
+						
+								img: "imageworship/igaveyoumyheart.jpg",
+								title: "I Gave You My Heart",
+								artist: "Hillsong Worship",
+								url: "lord-i-gave-you-my-heart.html"
+				},
+				{
+						
+								img: "imageworship/manalangin.png",
+								title: "Manalangin",
+								artist: "The Juans",
+								url: "10,000-reason.html"
+				},
+				{
+						
+								img: "imageworship/pag-ibig-lang.jpg",
+								title: "Pag-ibig Lang",
+								artist: "The Juans",
+								url: "pag-ibig.html"
+				},
+				{
+						
+								img: "imageworship/hopehascome.jpg",
+								title: "Hope Has Come",
+								artist: "Victory Worship",
+								url: "hope-has-come.html"
+				},
+				{
+						
+								img: "imageworship/timhughes.jpg",
+								title: "Here i am to Worship you",
+								artist: "Matt Redman",
+								url: "here-i-am-to-worship-you.html"
+				},
+				{
+						
+								img: "imageworship/healourland.jpg",
+								title: "Heal our Lord",
+								artist: "Jaime Rivera",
+								url: "heal-our-land.html"
+				},
+				{
+						
+								img: "imageworship/gary.jpg",
+								title: "Natutulog ba ang dyos?",
+								artist: "Gary Valenciano",
+								url: "natutulog-ba-ang-dyos.html"
+				},
+				{
+						
+								img: "imageworship/whoami.jpg",
+								title: "Who am I",
+								artist: "Casting Crowns",
+								url: "who-am-i.html"
+				}
+	]
+	
+	
+	let search_box = document.getElementById('input');
+	let search_bx2 = document.getElementsByClassName('search_bx2')[0];
+	
+window.addEventListener('load', () => {
+				song.forEach(element =>{
+								const { img, title, artist, url } = element;
+								
+								let card = document.createElement('a');
+								card.href = url;
+								card.innerHTML = `<img src="${img}">
+													<div class="artistName">
+													<h6>${title}</h6>
+													<p>${artist}</p>
+									</div>`;
+									search_bx2.appendChild(card);
+				});
+				
+});
+	          	search_box.addEventListener('keyup', () => {
+								let filter = search_box.value.toLowerCase();
+								let a = search_bx2.getElementsByTagName('a');
+								for (let i = 0; i < a.length; i++) {
+												let b = a[i].getElementsByClassName('artistName')[0];
+												let c = b.getElementsByTagName('h6')[0];
+												let d = b.getElementsByTagName('p')[0];
+												
+												let title = c.textContent || c.innerText;
+												let artist = d.textContent || d.innerText;
+												if (title.toLowerCase().indexOf(filter) > -1) {
+																a[i].style.display = "";
+																search_bx2.style.visibility = "visible";
+																search_bx2.style.opacity = 1;
+												 } else if (artist.toLowerCase().indexOf(filter) > -1) {
+																a[i].style.display = "";
+																search_bx2.style.visibility = "visible";
+																search_bx2.style.opacity = 1;
+												} else {
+																a[i].style.display = "none";
+												}
+												if (search_box.value == 0) {
+																search_bx2.style.visibility = "hidden";
+																search_bx2.style.opacity = 0;
+												}
+								}
+				});
